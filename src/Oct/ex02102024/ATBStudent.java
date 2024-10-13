a9fe9e45d13ddd5bf80e7a06c3afca8d5bd19c1d
govind dixit <govind.dixit045@gmail.com>
2024-10-13 09:10:29 +0530

    All the files 02 OCT

---
+++ src/Oct/ex02102024/ATBStudent.java
@@ -0,0 +1,30 @@
+package Oct.ex02102024;
+
+public class ATBStudent {
+
+    // Class- AtBStudent
+    // A - Attribute | data member | instance variable | member variable
+
+    long phone;
+    String email;
+    int age;
+    int batch8x;
+    String name;
+    boolean gender;
+    String address;
+    boolean isfeepaid;
+    String githublink;
+    String sid;
+
+//B - attrivute
+    void study(){}
+    void eat (){}
+    void walk (){}
+
+
+
+
+
+}
+
+
---
+++ src/Oct/ex02102024/Lab124.java
@@ -0,0 +1,33 @@
+package Oct.ex02102024;
+
+public class Lab124 {
+
+    public static void main(String[] args) {
+        // Object class -Oops
+        // String Class -Reff
+        // Arrays
+
+        // For each
+        // 2d nested
+
+        // Class --> blueprint to create something
+        // Object -Instance of the class - created from the class .
+
+        ATBStudent amit = new ATBStudent();
+        amit. age =35;
+
+        ATBStudent abhishek = new ATBStudent();
+        abhishek.age = 65;
+
+        System.out.println(amit); // Oct.ex02102024.ATBStudent@6acbcfc0
+
+        System.out.println(abhishek); //Oct.ex02102024.ATBStudent@5f184fc6
+
+        // abhishek - Ref Varaible - Byte, or bits, -- 8 Byte, 64 bites, value null default
+
+
+
+
+    }
+
+}
